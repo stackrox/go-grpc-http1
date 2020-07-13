a227af5cf1609fc2338b9c5671750d8ae8bf367c
Ross <ross@stackrox.com>
2020-07-13 11:24:08 -0700

    [ROX-5122] Use reusable buffer when reading from websocket (#18)

--- server/websocket_reader.go
+++ server/websocket_reader.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"io"
 
@@ -9,12 +10,11 @@ import (
 	"nhooyr.io/websocket"
 )
 
-// readResult stores the output from calls to (*wsReader).conn.Read
+// readerResult stores the output from calls to (*wsReader).conn.Reader
 // to be sent along a channel.
-type readResult struct {
-	mt  websocket.MessageType
-	msg []byte
-	err error
+type readerResult struct {
+	reader io.Reader
+	err    error
 }
 
 // wsReader is an io.ReadCloser that wraps around a WebSocket's io.Reader.
@@ -24,12 +24,20 @@ type wsReader struct {
 	currMsg []byte
 
 	// These are to prevent the WebSocket from closing due to
-	// (*websocket.Conn).Read's context potentially expiring.
+	// (*websocket.Conn).Reader's context potentially expiring.
 	// This can happen if Read waits indefinitely, which we prevent
-	// by decoupling the (*websocket.Conn).Read and Read.
+	// by decoupling the (*websocket.Conn).Reader and Read.
+	// The context is required to let readerLoop and Read know to stop.
 	readCtx       context.Context
 	readCtxCancel context.CancelFunc
-	readResultsC  chan readResult
+	readerResultC chan readerResult
+
+	// We use (*websocket.Conn).Reader instead of (*websocket.Conn).Read
+	// to remove the need for constant memory (de-)allocation when making
+	// a new buffer per read. Instead, we choose to manage a single buffer.
+	// The barrier is required to ensure there is only one reader used at-a-time.
+	buf      bytes.Buffer
+	barrierC chan struct{}
 
 	// Errors should be "sticky".
 	err error
@@ -37,27 +45,40 @@ type wsReader struct {
 
 func newWebSocketReader(ctx context.Context, conn *websocket.Conn) io.ReadCloser {
 	r := &wsReader{
-		ctx:          ctx,
-		conn:         conn,
-		readResultsC: make(chan readResult),
+		ctx:           ctx,
+		conn:          conn,
+		readerResultC: make(chan readerResult),
+		barrierC:      make(chan struct{}, 1),
 	}
+	r.barrierC <- struct{}{}
 	r.readCtx, r.readCtxCancel = context.WithCancel(r.ctx)
-	go r.readLoop()
+	go r.readerLoop()
 	return r
 }
 
-// readLoop continuously read messages off the WebSocket connection and forwards them along the results channel.
-func (r *wsReader) readLoop() {
+// readerLoop continuously obtains an io.Reader from the WebSocket connection and forwards each along the results channel.
+// There may only be one io.Reader at-a-time.
+func (r *wsReader) readerLoop() {
 	for {
-		mt, msg, err := r.conn.Read(r.ctx)
 		select {
-		case r.readResultsC <- readResult{
-			mt:  mt,
-			msg: msg,
-			err: err,
-		}:
 		case <-r.readCtx.Done():
 			return
+		case <-r.barrierC:
+		}
+
+		mt, reader, err := r.conn.Reader(r.ctx)
+		if err == nil && mt != websocket.MessageBinary {
+			err = errors.Errorf("incorrect message type; expected MessageBinary but got %v", mt)
+			reader = nil
+		}
+
+		select {
+		case <-r.readCtx.Done():
+			return
+		case r.readerResultC <- readerResult{
+			reader: reader,
+			err:    err,
+		}:
 		}
 	}
 }
@@ -75,35 +96,42 @@ func (r *wsReader) Read(p []byte) (int, error) {
 
 func (r *wsReader) doRead(p []byte) (int, error) {
 	if len(r.currMsg) == 0 {
+		var rr readerResult
 		select {
 		case <-r.readCtx.Done():
 			// CloseRead was called or the request's context expired.
 			// This is typically done in an error-case, only.
 			return 0, errors.Wrap(r.readCtx.Err(), "reading websocket message")
-		case rr := <-r.readResultsC:
-			if rr.err != nil {
-				return 0, rr.err
-			}
-			if rr.mt != websocket.MessageBinary {
-				return 0, errors.Errorf("incorrect message type; expected MessageBinary but got %v", rr.mt)
-			}
-
-			// Expect either an EOS message from the client or a valid data frame.
-			// Headers are not expected to be handled here.
-
-			if err := grpcproto.ValidateGRPCFrame(rr.msg); err != nil {
-				return 0, err
-			}
-			if grpcproto.IsEndOfStream(rr.msg) {
-				// This is where a connection without errors will terminate.
-				return 0, io.EOF
-			}
-			if !grpcproto.IsDataFrame(rr.msg) {
-				return 0, errors.Errorf("message is not a gRPC data frame")
-			}
-
-			r.currMsg = rr.msg
+		case rr = <-r.readerResultC:
 		}
+
+		if rr.err != nil {
+			return 0, rr.err
+		}
+
+		r.buf.Reset()
+		if _, err := r.buf.ReadFrom(rr.reader); err != nil {
+			return 0, err
+		}
+
+		// Allow (*wsReader).readerLoop to get a new reader.
+		r.barrierC <- struct{}{}
+
+		// Expect either an EOS message from the client or a valid data frame.
+		// Headers are not expected to be handled here.
+		msg := r.buf.Bytes()
+		if err := grpcproto.ValidateGRPCFrame(msg); err != nil {
+			return 0, err
+		}
+		if grpcproto.IsEndOfStream(msg) {
+			// This is where a connection without errors will terminate.
+			return 0, io.EOF
+		}
+		if !grpcproto.IsDataFrame(msg) {
+			return 0, errors.Errorf("message is not a gRPC data frame")
+		}
+
+		r.currMsg = msg
 	}
 
 	n := copy(p, r.currMsg)
@@ -112,11 +140,13 @@ func (r *wsReader) doRead(p []byte) (int, error) {
 	return n, nil
 }
 
-// Close signals the reader loops that we are no longer accepting messages.
+// Close signals readerLoop that we are no longer accepting messages.
 func (r *wsReader) Close() error {
 	// We cannot call (*websocket.Conn).CloseRead here. The WebSocket's closing handshake
-	// may not have been called yet, so the client may not know to stop sending messages,
-	// which may cause WebSocket errors.
+	// may not have been called yet, so the client may not know to stop sending messages.
+	// If the client sends a message after we call (*websocket.Conn).CloseRead, the WebSocket
+	// connection will return errors.
+	// Instead, we cancel our read context to signal all reads to stop.
 	r.readCtxCancel()
 	return nil
 }
