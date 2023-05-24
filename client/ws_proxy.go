c50baab4761ecb225b55cc61e3c5b944dcb64101
Dejan Lokar <dlokar@veza.com>
2023-05-24 11:09:53 +0200

    TMP

--- client/ws_proxy.go
+++ client/ws_proxy.go
@@ -233,6 +233,7 @@ func (h *http2WebSocketProxy) ServeHTTP(w http.ResponseWriter, req *http.Request
 	url.Host = h.endpoint
 
 	req.Header.Del("Content-Type")
+	req.Header.Set("Host", "tenant1.cookieai.test")
 
 	conn, resp, err := websocket.Dial(req.Context(), url.String(), &websocket.DialOptions{
 		// Add the gRPC headers to the WebSocket handshake request.
