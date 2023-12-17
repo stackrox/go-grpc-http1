12e0ec2b5ed046521fd83a51aa36151a79de2634
adonis <adonis.charalambidis@arisant.com>
2023-12-17 16:12:46 +0200

    http connect side channel dial proxy

--- client/side_channel_creds.go
+++ client/side_channel_creds.go
@@ -15,11 +15,15 @@
 package client
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"net"
+	"net/http"
+	"net/url"
 	"sync"
 
-	np "golang.org/x/net/proxy"
+	"golang.org/x/net/proxy"
 	"google.golang.org/grpc/credentials"
 )
 
@@ -48,7 +52,7 @@ func (c *sideChannelCreds) ClientHandshake(ctx context.Context, authority string
 		return rawConn, c.authInfo, nil
 	}
 
-	sideChannelConn, err := np.Dial(ctx, "tcp", c.endpoint)
+	sideChannelConn, err := proxy.Dial(ctx, "tcp", c.endpoint)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -61,4 +65,88 @@ func (c *sideChannelCreds) ClientHandshake(ctx context.Context, authority string
 
 	c.authInfo = authInfo
 	return rawConn, authInfo, nil
+}
+
+// httpProxy is a HTTP/HTTPS connect proxy.
+type httpProxy struct {
+	host     string
+	haveAuth bool
+	username string
+	password string
+	forward  proxy.Dialer
+}
+
+func newHTTPProxy(uri *url.URL, forward proxy.Dialer) (proxy.Dialer, error) {
+	s := new(httpProxy)
+	s.host = uri.Host
+	s.forward = forward
+	if uri.User != nil {
+		s.haveAuth = true
+		s.username = uri.User.Username()
+		s.password, _ = uri.User.Password()
+	}
+
+	return s, nil
+}
+
+func (s *httpProxy) Dial(network, addr string) (net.Conn, error) {
+	// Dial and create the https client connection.
+	c, err := s.forward.Dial("tcp", s.host)
+	if err != nil {
+		return nil, err
+	}
+
+	// HACK. http.ReadRequest also does this.
+	reqURL, err := url.Parse("http://" + addr)
+	if err != nil {
+		c.Close()
+		return nil, err
+	}
+	reqURL.Scheme = ""
+
+	req, err := http.NewRequest("CONNECT", reqURL.String(), nil)
+	if err != nil {
+		c.Close()
+		return nil, err
+	}
+	req.Close = false
+	if s.haveAuth {
+		req.SetBasicAuth(s.username, s.password)
+	}
+	// req.Header.Set("User-Agent", "Powerby Gota")
+
+	err = req.Write(c)
+	if err != nil {
+		c.Close()
+		return nil, err
+	}
+
+	resp, err := http.ReadResponse(bufio.NewReader(c), req)
+	if err != nil {
+		// TODO close resp body ?
+		resp.Body.Close()
+		c.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 200 {
+		c.Close()
+		err = fmt.Errorf("Connect server using proxy error, StatusCode [%d]", resp.StatusCode)
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func FromURL(u *url.URL, forward proxy.Dialer) (proxy.Dialer, error) {
+	return proxy.FromURL(u, forward)
+}
+
+func FromEnvironment() proxy.Dialer {
+	return proxy.FromEnvironment()
+}
+
+func init() {
+	proxy.RegisterDialerType("http", newHTTPProxy)
+	proxy.RegisterDialerType("https", newHTTPProxy)
 }
